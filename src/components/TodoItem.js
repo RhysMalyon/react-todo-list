8dd049d0ee600c9e5a7944fde834712aa128a8d8
Rhys Malyon <malyonr@gmail.com>
2021-10-04 21:11:30 +0900

    Create TodoItem component

---
+++ src/components/TodoItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class TodoItem extends Component {
+  constructor() {
+    super()
+    this.state = {
+      // STATE HERE
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default TodoItem
